5a7b501a0e944731f8dc3542f26bf048008f46bf
laboratorio <laboratorio@UPEAULA-31716>
2024-10-18 07:16:10 -0500

    Agregar archivos de proyecto.

---
+++ SAbadTaller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SAbadTaller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
